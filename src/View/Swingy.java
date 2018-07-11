23bafbd65a975be32215dba8c0d3c18416a24601
spencerschafer <spencerschafer@gmail.com>
2018-07-11 14:30:14 +0200

    STABLE : Add Model-View-Controller packages
    - Add packages referencing MVC design pattern
    - Haven't implemented much, but have laid out program and its requirements on Trello

---
+++ src/View/Swingy.java
@@ -0,0 +1,12 @@
+package View;
+
+import javax.swing.*;
+
+public class Swingy {
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Swingy");
+        frame.setSize(300, 300);
+        frame.setLocationRelativeTo(null); // place window in the center of the screen
+        frame.setVisible(true);
+    }
+}
